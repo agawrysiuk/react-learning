8e81d7762697f6d5e99f212336e2973a301b6738
dlrzz <arkad.gawry@gmail.com>
2021-03-29 11:51:56 +0200

    Creating new components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
-  render() {
-    return (
-        // our JSX element must have exactly one root element -> div here
-        // it's best to wrap everything in one root component
-        // class is a reserved word in JSX, that's why we use className
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work?"));
-  }
+    render() {
+        return (
+            // our JSX element must have exactly one root element -> div here
+            // it's best to wrap everything in one root component
+            // class is a reserved word in JSX, that's why we use className
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <Person name="Max"/>
+                <Person name="Manu" age="29">I like racing!</Person>
+                <Person name="Stephanie" age="26"/>
+            </div>
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work?"));
+    }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// in a simplest form, component is a function that returns a JSX
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm a {props.name} and with age of: {props.age ? props.age : getRandomNumber()}!</p>
+            {/* this is a way to make a comment in the JSX */}
+            {/* below, we pass elements between the opening and closing tags of our component if they exist (no empty <p> tags) */}
+            {props.children ? <p>{props.children}</p> : null}
+        </div>
+    );
+}
+
+const getRandomNumber = () => Math.floor(Math.random() * 30);
+
+export default person;
