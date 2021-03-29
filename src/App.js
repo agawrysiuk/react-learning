945575b0611a45ca8c04c4e8c55a846fdd7ac31f
dlrzz <arkad.gawry@gmail.com>
2021-03-29 11:19:03 +0200

    Adding information about JSX and render() method

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work?"));
+    return (
+        // our JSX element must have exactly one root element -> div here
+        // it's best to wrap everything in one root component
+        // class is a reserved word in JSX, that's why we use className
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work?"));
   }
 }
 
