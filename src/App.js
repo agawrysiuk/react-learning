47092ce12f2b4413b924070a488be11da9d04a43
dlrzz <arkad.gawry@gmail.com>
2021-03-29 14:48:09 +0200

    Using props with one-way binding of state and methods

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+    // state is only available in classes that extend Component
+    // (unless it's React Hooks available from 16.8, then it's also available in functional components)
+    // it's manageable only from inside
+    // state is a special property because it's the only one that forces React to rerender/update our DOM when it changes
+    state = {
+        persons: [
+            {name: "Max", age: undefined},
+            {name: "Manu", age: 28},
+            {name: "Stephanie", age: 26}
+        ],
+        otherState: 'some other value'
+    }
+
+    switchNameHandler = newName => {
+        // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+        // in a regular setState(), like below, the old data will remain and only the new fields will be updated
+        // in the useState(model, functionToUpdate) Hook for React, the functionToUpdate() overrides model used previously in useState()!
+        // but you can call useState() several times with different models and different functions to update it
+        this.setState({
+            persons: [
+                {name: newName, age: undefined},
+                {name: "Manu", age: 28},
+                {name: "Stephanie", age: 22}
+            ]
+        })
+    }
+
     render() {
         return (
             // our JSX element must have exactly one root element -> div here
@@ -10,9 +37,18 @@ class App extends Component {
             // class is a reserved word in JSX, that's why we use className
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <Person name="Max"/>
-                <Person name="Manu" age="29">I like racing!</Person>
-                <Person name="Stephanie" age="26"/>
+                {/* One way to pass function with properties, but may be inneficient performance-wise, better use .bind() */}
+                <button onClick={() => this.switchNameHandler("ASD")}>Switch name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    click={this.switchNameHandler.bind(this, "Maximilian")}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}>I like racing!</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                    click={this.switchNameHandler.bind(this, "Joseph")}/>
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work?"));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 // in a simplest form, component is a function that returns a JSX
-const person = (props) => {
+const Person = (props) => {
     return (
         <div>
-            <p>I'm a {props.name} and with age of: {props.age ? props.age : getRandomNumber()}!</p>
+            <p onClick={props.click}>I'm a {props.name} and with age of: {props.age ? props.age : getRandomNumber()}!</p>
             {/* this is a way to make a comment in the JSX */}
             {/* below, we pass elements between the opening and closing tags of our component if they exist (no empty <p> tags) */}
             {props.children ? <p>{props.children}</p> : null}
@@ -14,4 +14,4 @@ const person = (props) => {
 
 const getRandomNumber = () => Math.floor(Math.random() * 30);
 
-export default person;
+export default Person;
